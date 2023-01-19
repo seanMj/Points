b39de7ac054b978a7a55aa4a62676306d633f208
seanMj <74737560+seanMj@users.noreply.github.com>
2023-01-19 11:35:44 -0700

    Update math.hpp
    updated my concerns, canged y_itercept to __b

--- math.hpp
+++ math.hpp
@@ -10,8 +10,12 @@
 #define NDEBUG
 
 template<typename T> T slope(points<T>& Pts);
-template<typename T> T y_intercept(points<T>& pts);
+template<typename T> T __b(points<T>& pts);
+
+//idk what is function is for, i did the equation in the book... 
+//but since i was never taught math, and im self taught in C++ im just currently confused
 template<typename T> T slope_intercept(points<T>& pts);
+
 template<typename T> T distance(points<T>& pts);
 template<typename T> void midpoint(points<T>& pts, T& Mx,  T& My);
 
